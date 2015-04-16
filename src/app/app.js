69d1e830163131eeb32e81d4f4839b8a6c0b251d
Prateek Sachdeva <prateeksach@gmail.com>
2015-04-16 14:43:02 -0400

    documentation added everywhere

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,10 @@
+// Parse Keys and Initialization
 var PARSE_APP_ID = 'gprSlTIj7xJnYyRbgzmKUwSyqL91TloH5W0MHRGR';
 var PARSE_JAVASCRIPT_KEY = 'NbL7T1SwgJLlntQMCahzBL7XJuktYEzuCudKmWI2';
 
 Parse.initialize(PARSE_APP_ID, PARSE_JAVASCRIPT_KEY);
 
-function trackEvent(name, options) {
-  Parse.Analytics.track(name, options);
-}
-
+// Universal Functions
 function validateEmail(email) { 
   var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
@@ -24,6 +22,7 @@ moment.locale('en', {
   }
 });
 
+// Define main app module
 angular.module( 'ngBoilerplate', [
   'templates-app',
   'templates-common',
@@ -32,21 +31,26 @@ angular.module( 'ngBoilerplate', [
   'ui.router'
 ])
 
+// Define default URL
 .config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
   $urlRouterProvider.otherwise( '/home' );
 })
 
+// Main app initialization
 .run( function run () {
 
 })
 
+// Super controller
 .controller( 'AppCtrl', function AppCtrl ( $rootScope, $scope, $location ) {
+  // Set page title whenever the URL changes
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     if ( angular.isDefined( toState.data.pageTitle ) ) {
       $scope.pageTitle = toState.data.pageTitle + ' | MBooks' ;
     }
   });
 
+  // Check if a user is logged in
   $rootScope.validateUser = function() {
     var user = Parse.User.current();
     if(!user || !user.authenticated()) 
@@ -55,20 +59,21 @@ angular.module( 'ngBoilerplate', [
     return true;
   }
 
+  // Nav bar collapsing for responsive usage
   $scope.navBarExpanded = false;
-  
   $scope.toggleNavBarExpansion = function() {
     $scope.navBarExpanded = !$scope.navBarExpanded;
   }
 
+  // Show login modal
   $scope.showLoginModal = function() {
 
   }
 
+  // Show signup modal
   $scope.showSignupModal = function() {
     
   }
 })
 
 ;
-
